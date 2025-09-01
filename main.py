06719cd8f94b3c640b7c340c0760dfe59ab41a90
gcotelli86 <gcotelli86@gmail.com>
2025-09-01 22:47:16 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot attivo su Railway 🚀")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
